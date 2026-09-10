35f79ce54e681ff1310d65c3a3f75d2f6fe686a1
waleed633 <abdullahkhan1772002@gmail.com>
2026-09-10 19:51:16 +0500

    Add files via upload

---
+++ lesson 1/hello world.py
@@ -0,0 +1,13 @@
+# print a sentence
+print("Hello, World!")
+
+# print a Number
+print(4)
+
+# Next Line after print
+print("Hello python \n")
+
+# Print all the keyywords present in python
+print("keywords in python are....")
+import keyword
+print(keyword.kwlist)
